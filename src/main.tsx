ff75fd3e0e7c0ab65bb34b21092270f4a849deef
EriickChavez <10erickchavez10@gmail.com>
2026-06-08 15:51:35 -0600

    refactor: expose components to globalThis instead of window and remove unused CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
-import "./index.css";
 import { MonitorScreen } from "./components/MonitorScreen";
 import { MonitorWidget } from "./components/MonitorWidget";
 
-// Solo exponemos los componentes.
-// Nexabook ya tiene su propio 'root' y él los renderizará donde quiera.
-window["com.nexabook.modulo-pro"] = {
-  MonitorScreen,
-  MonitorWidget,
+// Esto hace que el Hub pueda "ver" tus componentes
+(globalThis as any)["com.nexabook.modulo-pro"] = {
+  MonitorScreen: MonitorScreen,
+  MonitorWidget: MonitorWidget,
 };
-
-console.log("Módulo cargado y componentes expuestos a Nexabook.");
